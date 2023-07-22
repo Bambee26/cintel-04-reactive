0000e58c85b2c53c5eb41838cae6276103861c0b
Bambee Garfield <bambeegarfield@gmail.com>
2023-07-22 15:04:50 -0700

    Widget Thingies

--- mtcars_ui_inputs.py
+++ mtcars_ui_inputs.py
@@ -1,47 +1,38 @@
 """
-Purpose: Provide user interaction options for MT Cars dataset.
+Purpose: Provide user interaction options for the Cars dataset.
+
+ - Choose checkboxes when the options are independent of each other.
+ - Choose radio buttons when a set of options are mutually exclusive.
 
 IDs must be unique. They are capitalized in this app for clarity (not typical).
 The IDs are case-sensitive and must match the server code exactly.
 Preface IDs with the dataset name to avoid naming conflicts.
 
 """
+
 from shiny import ui
 
 
 def get_mtcars_inputs():
     return ui.panel_sidebar(
-        ui.h2("MT Cars Interaction"),
+        ui.h2("Cars Interaction"),
         ui.tags.hr(),
         ui.input_slider(
             "MTCARS_MPG_RANGE",
-            "Miles Per Gallon (MPG)",
+            "Miles per Gallon (MPG)",
             min=10,
             max=35,
             value=[10, 35],
         ),
-        ui.input_numeric(
-            "MTCARS_hp", 
-            "Gross Horsepower:", 
-            value=335.0),
-        ui.tags.hr(),
-        ui.tags.section(
-            ui.h3("MT Cars Table"),
-            ui.tags.p("Description of each field in the table:"),
-            ui.tags.ul(
-                ui.tags.li("mpg: Miles per Gallon"),
-                ui.tags.li("cyl: Number of cylinders"),
-                ui.tags.li("disp: Displacement (cubic inches)"),
-                ui.tags.li("hp: Gross horsepower"),
-                ui.tags.li("drat: Rear axle ratio"),
-                ui.tags.li("wt: Weight (1,000 lbs)"),
-                ui.tags.li("qsec: 1/4 mile time"),
-                ui.tags.li("vs: V/S (Engine shape; 0 = V-shaped, 1 = Straight)"),
-                ui.tags.li("am: Transmission (0 = Automatic, 1 = Manual)"),
-                ui.tags.li("gear: Number of forward gears"),
-                ui.tags.li("carb: Number of carburetors"),
-            ),
-            ui.output_table("cars_table"),
+        ui.input_numeric("MTCARS_HP", "Gross horsepower:", value=335.0),
+        ui.input_checkbox("MTCARS_GEARS_3", "3", value=True),
+        ui.input_checkbox("MTCARS_GEARS_4", "4", value=True),
+        ui.input_checkbox("MTCARS_GEARS_5", "5", value=True),
+        ui.input_radio_buttons(
+            "MTCARS_TRANS",
+            "Select number of gears",
+            {"a": "Automatic", "m": "Manual"},
+            selected="a",
         ),
         ui.tags.hr(),
         ui.p("🕒 Please be patient. Outputs may take a few seconds to load."),
